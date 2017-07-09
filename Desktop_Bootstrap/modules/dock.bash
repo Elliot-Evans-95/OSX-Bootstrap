fad0e1e82f555e04555c9879cbeaf50e26079a6c
Elliot-Evans-95 <ell15evans.nuls@googlemail.com>
2017-07-09 19:12:31 +0100

    commented out Jetbrains apps as they will change do you how they are installed

--- Desktop_Bootstrap/modules/dock.bash
+++ Desktop_Bootstrap/modules/dock.bash
@@ -13,26 +13,27 @@ echo
 dockutil --remove all --allhomes
 
 # Add Specific apps to Dock
-dockutil --add /Applications/Google%20Chrome.app --allhomes
+dockutil --add "/Applications/Google Chrome.app" --allhomes
 dockutil --add /Applications/Hyper.app --allhomes
 dockutil --add /Applications/Transmit.app --allhomes
-dockutil --add /Library/Application Support/JetBrains/Toolbox/apps/WebStorm/ch-0/163.15188.8/WebStorm --allhomes
-dockutil --add /Library/Application Support/JetBrains/Toolbox/apps/PhpStorm/ch-0/163.13906.21/PhpStorm --allhomes
-dockutil --add /Library/Application Support/JetBrains/Toolbox/apps/Gogland/ch-0/171.4424.55/Gogland 1.0 EAP--allhomes
+#dockutil --add "~/Library/Application\ Support/JetBrains/Toolbox/apps/WebStorm/ch-0/171.4424.63/WebStorm.app" --allhomes
+#dockutil --add "~/Library/Application\ Support/JetBrains/Toolbox/apps/PhpStorm/ch-0/163.13906.21/PhpStorm.app" --allhomes
+#dockutil --add "~/Library/Application\ Support/JetBrains/Toolbox/apps/Gogland/ch-0/171.4424.55/Gogland 1.0 EAP.app" --allhomes
 dockutil --add /Applications/Sketch.app --allhomes
 dockutil --add /Applications/Discord.app --allhomes
 dockutil --add /Applications/Slack.app --allhomes
-dockutil --add /Applications/Fantastical%202.app --allhomes
+dockutil --add "/Applications/Fantastical 2.app" --allhomes
 dockutil --add /Applications/OmniFocus.app --allhomes
 dockutil --add "~/Downloads" --section others --display stack --allhomes
 
 # Add spacing
-dockutil --add '' --type spacer --section apps --after Google%20Chrome
+dockutil --add '' --type spacer --section apps --after Chrome
 dockutil --add '' --type spacer --section apps --after Hyper
 dockutil --add '' --type spacer --section apps --after Transmit
-dockutil --add '' --type spacer --section apps --after Goland%201.0%20EAP
+#dockutil --add '' --type spacer --section apps --after Goland 1.0 EAP
 dockutil --add '' --type spacer --section apps --after Sketch
 dockutil --add '' --type spacer --section apps --after Slack
+dockutil --add '' --type spacer --section apps --after OmniFocus
 
 echo
 echo "********************* Added Apps to Dock *****************************"
