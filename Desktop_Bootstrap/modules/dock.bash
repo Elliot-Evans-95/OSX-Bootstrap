6b00ac4b1243ae6f21dee16261ad1b2728ca68c2
Elliot-Evans-95 <ell15evans.nuls@googlemail.com>
2017-07-09 18:16:29 +0100

    now using Dockutil for the dock script

--- Desktop_Bootstrap/modules/dock.bash
+++ Desktop_Bootstrap/modules/dock.bash
@@ -6,18 +6,34 @@ echo "****              Adding Applications to Dock.                    ****"
 echo "**********************************************************************"
 echo
 
-defaults write com.apple.dock persistant-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-killall -HUP Dock
+# NEED TO INSTALL DOCKUTIL
+# https://github.com/kcrawford/dockutil/releases/tag/2.0.5
 
-defaults write com.apple.dock persistant-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Hyper.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-killall -HUP Dock
+# Removes All Dock Applications
+dockutil --remove all --allhomes
 
-defaults write com.apple.dock persistant-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Sketch.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-killall -HUP Dock
+# Add Specific apps to Dock
+dockutil --add /Applications/Google_Chrome.app --allhomes
+dockutil --add /Applications/Hyper.app --allhomes
+dockutil --add /Applications/Transmit.app --allhomes
+dockutil --add /Applications/Webstorm.app --allhomes
+dockutil --add /Applications/PHPStorm.app --allhomes
+dockutil --add /Applications/Goland.app --allhomes
+dockutil --add /Applications/Sketch.app --allhomes
+dockutil --add /Applications/Discord.app --allhomes
+dockutil --add /Applications/Slack.app --allhomes
+dockutil --add /Applications/Fantastical_2.app --allhomes
+dockutil --add /Applications/OmniFocus.app --allhomes
+dockutil --add "~/Downloads" --section others --display stack --allhomes
 
-defaults write com.apple.dock persistant-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Safari.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-killall -HUP Dock
+# Add spacing
+dockutil --add '' --type spacer --section apps --after Google_Chrome.app
+
+# Orginise Dock to preverance
 
 echo
+echo "********************* Added Apps to Dock *****************************"
+echo " Finder | Chrome | - | Hyper | - | Transmit | - | Webstorm | PHPstorm | GOLand | - |"
+echo " Sketch | - | Discord | Slack | - | Fantastical_2 | OmniFocus || - | Downloads | - | Bin"
 echo "********************* Added Apps to Dock *****************************"
 echo
