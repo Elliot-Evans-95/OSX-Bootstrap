40bf1c0406cfa7fa28c13b7eb56fa57102845e9a
Elliot-Evans-95 <ell15evans.nuls@googlemail.com>
2017-07-21 21:21:41 +0100

    removed info_echo

--- Laptop_Bootstrap/modules/osx_security.bash
+++ Laptop_Bootstrap/modules/osx_security.bash
@@ -8,12 +8,12 @@ echo "****              Configurating OSX Security.                     ****"
 echo "**********************************************************************"
 echo
 
-info_echo "Run osxlockdown"
+echo "Run osxlockdown"
 sudo vendor/osxlockdown/osxlockdown -remediate -commands_file vendor/osxlockdown/commands.yaml
 
-info_echo "Expose hidden files and Library folder in Finder"
+echo "Expose hidden files and Library folder in Finder"
 defaults write com.apple.finder AppleShowAllFiles -bool true
 chflags nohidden ~/Library
 
-info_echo "Empty Trash securely by default"
+echo "Empty Trash securely by default"
 defaults write com.apple.finder EmptyTrashSecurely -bool true
